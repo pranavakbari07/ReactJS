9543ca27da5962a2daf04604e9281610995b0be9
Pranav Akbari <pranavakbari07@gmail.com>
2025-09-01 11:37:36 +0530

    project

---
+++ Food lover Web Using ReactJS/Food lover/src/Components/Nav.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+      <div class="home">
+           <nav>
+            <div class="nav1">
+                <h3 class="h">HOME</h3>
+                <h3>ABOUT</h3>
+                <h3>MENUS</h3>
+                <h3>OFFERS</h3>
+            </div>
+            <div class="nav2">
+                <img src="logo.png" alt=""/>
+            </div>
+            <div class="nav3">
+                <h3>GALLERY</h3>
+                <h3>CONTACT</h3>
+                <h3>BLOG</h3>
+            </div>
+           </nav>
+           <div class="brack">
+            <div class="img1">
+                <img src="Layer 2 copy (1).png" alt=""/>
+            </div>
+            <div class="img2">
+                <h1>Contact Us For <br/>Best Breakfasr</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit, sed do eiusmod tempor <br/> incididunt ut labore et dolore magna.</p>
+                <button>Contact US</button>
+            </div>
+           </div>
+        </div>
+    </div>
+  )
+}
